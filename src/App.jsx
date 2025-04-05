a2cf307f76af8a775434284a5c25a3f50496e2e1
imole <dremkay71@gmail.com>
2025-04-05 14:11:20 +0100

    react vite initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>imole</h1>
+        <h3>imole</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
